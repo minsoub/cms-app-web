f3a8bf0a7668c346907466c118d14a9c90d52171
fe_user_01-at-807380035085 <junhoh0102@bithumbsysttems.com>
2022-12-28 11:28:42 +0900

    헤더 작업

--- src/components/View/Header/index.tsx
+++ src/components/View/Header/index.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import './Header.scss';
 
 const Header = () => {
     const navigate = useNavigate();
@@ -10,7 +11,7 @@ const Header = () => {
 
     return (
         <header>
-            <button type="button" onClick={handleBack}>뒤로가기</button>
+            <button type="button"  onClick={handleBack} className="button-back">뒤로가기</button>
             <h1>빗썸 공지사항</h1>
         </header>
     )
