a7f4fee4fe8cccb35687918abc114479b1fa1b37
leehyunju <hyunmail521@bithumbsystems.com>
2022-12-29 17:10:02 +0900

    d

--- src/pages/noticeView/index.tsx
+++ src/pages/noticeView/index.tsx
@@ -1,9 +1,8 @@
-import Header from "components/View/Header";
-import {useEffect, useState} from "react";
-import fetcher from "lib/api";
-import { METHOD, IPath } from "lib/type";
-import { useParams } from "react-router-dom";
-
+import Header from 'components/View/Header';
+import { useEffect, useState } from 'react';
+import fetcher from 'lib/api';
+import { METHOD, IPath } from 'lib/type';
+import { useParams } from 'react-router-dom';
 
 // 공지사항 Detail 페이지
 const NoticeView = () => {
@@ -13,18 +12,18 @@ const NoticeView = () => {
     const [htmlCode, setHtmlCode] = useState<string>('');
     const [modify, setModify] = useState<boolean>(false);
 
-    console.log('boardPath 입니다--->boardPath', boardPath)
+    console.log('boardPath 입니다--->boardPath', boardPath);
     const getView = async () => {
         const res = await fetcher(METHOD.GET, `/v1/api/cms/notice/detail/:${boardPath}`);
-        setTitle(res.data.title)
-        setDate(res.data.create_date)
-        setHtmlCode(res.data.content)
-        setModify(res.data.isUpdate)
-    }
+        setTitle(res.data.title);
+        setDate(res.data.create_date);
+        setHtmlCode(res.data.content);
+        setModify(res.data.isUpdate);
+    };
 
     useEffect(() => {
-        getView()
-    }, [])
+        getView();
+    }, []);
 
     return (
         <main>
@@ -34,12 +33,11 @@ const NoticeView = () => {
                 <h2>{title}</h2>
                 <p>{date}</p>
                 {modify === true && <p>수정됨</p>}
-
             </div>
 
-            <article dangerouslySetInnerHTML={{ __html: htmlCode}}/>
+            <article dangerouslySetInnerHTML={{ __html: htmlCode }} />
         </main>
-    )
-}
+    );
+};
 
 export default NoticeView;
