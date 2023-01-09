3955d0fdf9497ae4f3c28c1c81cbc09f64f9e14b
leehyunju <hyunmail521@bithumbsystems.com>
2023-01-09 21:18:46 +0900

    path 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
-import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
-import NoticeList from './pages/noticeList'
-import NoticeView from './pages/noticeView'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NoticeList from './pages/noticeList';
+import NoticeView from './pages/noticeView';
 import './App.css';
 
 function App() {
-    let { boardPath } = useParams();
-
-  return (
-      <RecoilRoot>
-          <Router>
-              <Routes>
-                  <Route path="/notice" element={<NoticeList />} />
-                  <Route path="/notice/:boardPath" element={<NoticeView />} />
-                  {/* 404 페이지 */}
-                  {/*<Route path="/*" element={<NoData />} />*/}
-              </Routes>
-          </Router>
-      </RecoilRoot>
-  );
+    return (
+        <RecoilRoot>
+            <Router>
+                <Routes>
+                    <Route path="/notice" element={<NoticeList />} />
+                    <Route path="/notice/:boardID" element={<NoticeView />} />
+                    {/* 404 페이지 */}
+                    {/*<Route path="/*" element={<NoData />} />*/}
+                </Routes>
+            </Router>
+        </RecoilRoot>
+    );
 }
 
 export default App;
--- src/components/List/Item/index.tsx
+++ src/components/List/Item/index.tsx
@@ -1,15 +1,26 @@
-import { IItemProps } from 'lib/type';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-const Item = ({title, date, id}:IItemProps) => {
+interface INoticeListProps {
+    id: string; // 게시글 id
+    title: string; // 제목
+    createDate: string; // 작성일
+}
+
+/**
+ * @param title {string}
+ * @param createDate {string}
+ * @param id {string}
+ * @constructor
+ */
+const Item = ({ title, createDate, id }: INoticeListProps) => {
     return (
         <li key={id}>
-            <Link to={`/notice/:${id}`}>
+            <Link to={`/notice/${id}`}>
                 <h4>{title}</h4>
-                <p>{date}</p>
+                <p>{createDate}</p>
             </Link>
         </li>
-    )
-}
+    );
+};
 
 export default Item;
