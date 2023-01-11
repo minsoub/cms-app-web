3bfbc7271947f2705d7aece0569230ef403c0b14
fe_user_01-at-807380035085 <junhoh0102@bithumbsysttems.com>
2023-01-11 09:53:01 +0900

    Reafactor: 장애페이지 작업 오타및 css

--- src/components/List/Pagination/Arrows/LeftArrow.tsx
+++ src/components/List/Pagination/Arrows/LeftArrow.tsx
@@ -3,7 +3,7 @@ import { SVGProps } from 'react';
 
 const LeftArrow = (props: SVGProps<SVGSVGElement>) => {
     return (
-        <svg width={24} height={24} viewBox="0 0 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
             <title>이전 페이지</title>
             <path d="M15 19L8 12L15 5" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
         </svg>
